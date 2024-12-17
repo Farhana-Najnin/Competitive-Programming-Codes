b38e760480239819b5a8ee29103f7fd257671105
Farhana Najnin mahi <129548085+Farhana-Najnin@users.noreply.github.com>
2024-12-17 11:53:55 +0600

    Add files via upload

---
+++ LightOJ/Opposite Task.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+int t;
+scanf("%d",&t);
+while(t--){
+    int n;
+    scanf("%d",&n);
+    int a=n/2;
+    int b=n-a;
+    printf("%d %d\n",a,b);
+}
+return 0;
+}
